6ebee54ed05124d01e0e6de2dddb27ac7fba2d93
zhanchao <1343145329@qq.com>
2019-12-09 22:27:14 +0800

    base

---
+++ cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/main.cpp.o"
+  "Lab2.exe"
+  "Lab2.pdb"
+  "libLab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
